df348b45e5f060210c8f2fd3418806262f613270
Fbarata03 <felibarata03@gmail.com>
2026-04-08 13:28:32 +0100

    feat: adicionar service worker e melhorar manifesto PWA para Play Store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {});
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
