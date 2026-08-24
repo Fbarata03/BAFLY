10b502f49118471fc04838b1e9267c65bb9613b1
Feliciano Barata <felibarata03@gmail.com>
2026-08-24 10:10:44 +0100

    Set basename for BrowserRouter to '/BAFLY'

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ if ('serviceWorker' in navigator) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/BAFLY">
     <App />
   </BrowserRouter>,
 )
